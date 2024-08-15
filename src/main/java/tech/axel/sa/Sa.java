2cb4596182f4cf6560fa3fe0bc3b53e8e8e33eee
Axel Kangou <axel@insight-hq.com>
2024-08-15 17:47:18 +0000

    Projet initial

---
+++ src/main/java/tech/axel/sa/Sa.java
@@ -0,0 +1,8 @@
+package tech.axel.sa;
+
+public record Sa(int id ,
+String message,
+int status) {
+    
+
+}
---
+++ src/test/java/tech/axel/sa/SaAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.axel.sa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
